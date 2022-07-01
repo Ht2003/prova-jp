439cc9559aac85d64cbe8d16588d7bcad71c0252
HILIZANGELA TALLIA <105807771+Ht2003@users.noreply.github.com>
2022-07-01 19:53:00 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-#fiz a importaçao dessa bibioteca no python para o programa nao aceitar numeros,
-#apenas letras, toda vez que o usuario digitar um numero,ele vai ser removido
-from string import digits
-string=input('digite alguma coisa').strip()#vai eliminar os espaços que o usuario digiter
-table=str.maketrans('','', digits)#vai fazer a remoçao dos numeros
-newstring= string.translate(table)
+#criaçao da funcao nome bibliografico
+#transformei o nome em uma lista para usar os comandos de decisao para aceitar ate 3 nomes
+# e depois inverti as posiçoes no print,se a lista tiver 3 elementos,2 ou 1 ela vai sair com as
+# posiçoes invertidas
+#chemei a funçao e pedi pra ela retornar o nome bibliografico
+#usei o upper para deixar o nome maiusculo
+def nome_bibliografico(Nome):
+    if len(Nome) == 2:
+       print(Nome[-1].upper(),',',Nome[0])
+    elif len(Nome) == 3:
+       print(Nome[-1].upper(),',',Nome[0],Nome[-2])
+    elif len(Nome) == 1:
+        print(Nome[-0].upper())
 
-v='aeiou-AEIOU'#criaçao de duas variaveis para serem comparadas
-c='bcdfghjklmnpqrstvwxyz-BCDFGHJKLMNPQRSTSVWXYZ'
+Nome=input('digite o nome').split()
+nome_bibliografico(Nome)
+titulo=input('digite o titulo do livro')
+nume=input('digite o numero do exemplar:')
+endereco=input('digite o enderço')
+editora=input('digite o nome da editora')
+ano=input('digite o ano de publicaçao do livro')
+print(f'{nome_bibliografico},{titulo},{nume},{editora},{ano}')
 
-for letra in string :#processo para deixar letras maiusculas e outras minusculas
-    if letra in v:#se for uma  vogal vai sair maiuscula
-      print(letra.upper(), end='')
-    elif letra in c:# se a letra for uma consoante vai sair minuscula,caso o usuario digite maiuscula,vai ficar maiuscula
-       print(letra, end='')
+
+
+
+
+
+
